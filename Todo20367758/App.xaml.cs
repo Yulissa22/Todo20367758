8e2a0ca6437be8fb5df2d51208419f87f923f30e
Yulissa22 <ec1413220@gmail.com>
2023-08-20 18:36:48 -0600

    Agregar archivos de proyecto.

---
+++ Todo20367758/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Todo20367758;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Todo20367758/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Todo20367758.Views;
+
+namespace Todo20367758;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+		Routing.RegisterRoute(nameof(TodoItemPage), typeof(TodoItemPage));
+	}
+}
